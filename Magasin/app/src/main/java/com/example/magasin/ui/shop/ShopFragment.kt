e8a2102c98f1ec7120920ed289d411d90c728ebd
EduFelixDallaire <fedal001@edu.cegepgarneau.ca>
2024-10-02 13:30:32 -0400

    start implementing shop view and shop_item

--- Magasin/app/src/main/java/com/example/magasin/ui/shop/ShopFragment.kt
+++ Magasin/app/src/main/java/com/example/magasin/ui/shop/ShopFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.magasin.databinding.FragmentShopBinding
 
 class ShopFragment : Fragment() {
@@ -22,17 +20,9 @@ class ShopFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val shopViewModel =
-            ViewModelProvider(this).get(shopViewModel::class.java)
-
+        // Inflate the layout for this fragment using view binding
         _binding = FragmentShopBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textShop
-        shopViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
